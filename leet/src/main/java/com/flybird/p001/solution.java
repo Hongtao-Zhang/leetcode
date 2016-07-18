72a7d25ecc23e3bd3533c12a5aab9dc13e998420
hongtao <hzhang@microstrategy.com>
2016-07-18 13:59:56 -0400

    Initial solution which failed

--- leet/src/main/java/com/flybird/p001/solution.java
+++ leet/src/main/java/com/flybird/p001/solution.java
@@ -1,7 +1,33 @@
 package com.flybird.p001;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by hzhang.
  */
 public class solution {
+
+    public int[] twoSum(int[] nums, int target) {
+        if (nums.length < 2)
+            return null;
+
+        Map<Integer, Integer> elementMap = new HashMap<Integer, Integer>(nums.length);
+        elementMap.put(nums[0], 0);
+
+        for(int i=1; i<nums.length; i++) {
+            // first check if there is an matching element exists
+            int targetValue = target - nums[i];
+            if (elementMap.containsKey(targetValue)) {
+                int[] result = new int[2];
+
+                result[0] = elementMap.get(targetValue);
+                result[1] = i;
+
+                return result;
+            }
+        }
+
+        return null;
+    }
 }
