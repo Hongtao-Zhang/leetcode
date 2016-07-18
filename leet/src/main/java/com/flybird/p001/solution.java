f010c85d0425e99d7352d61a8f8fc79661c9238c
hongtao <hzhang@microstrategy.com>
2016-07-18 12:00:14 -0400

    Init Project

---
+++ leet/src/main/java/com/flybird/p001/solution.java
@@ -0,0 +1,7 @@
+package com.flybird.p001;
+
+/**
+ * Created by hzhang.
+ */
+public class solution {
+}
---
+++ leet/src/test/java/com/flybird/p001/solutionTest.java
@@ -0,0 +1,10 @@
+package com.flybird.p001;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by hzhang.
+ */
+public class solutionTest {
+
+}
