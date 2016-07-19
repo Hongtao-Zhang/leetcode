6714b542dec2e1671a2792258d3b241ae65f1965
hongtao <hongtao.zhang@gmail.com>
2016-07-19 10:43:21 -0400

    p3 correct 2

---
+++ leet/src/main/java/com/flybird/p004/Solution.java
@@ -0,0 +1,18 @@
+package com.flybird.p004;
+
+/**
+ * Created by hongtao.
+ *
+ * Basically we want to remove half of one array during each step
+ */
+public class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int start1 = 0;
+        int end1 = nums1.length;
+        int start2 = 0;
+        int end2 = nums2.length;
+
+
+
+    }
+}
