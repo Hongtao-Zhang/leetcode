c890bb3f504713740dc7c0b45269b35bfcadbff3
hongtao <hzhang@microstrategy.com>
2016-07-22 14:48:46 -0400

    problem 3

--- leet/src/main/java/com/flybird/p004/Solution.java
+++ leet/src/main/java/com/flybird/p004/Solution.java
@@ -3,16 +3,20 @@ package com.flybird.p004;
 /**
  * Created by hongtao.
  *
- * Basically we want to remove half of one array during each step
+ * Basically we want to remove half of one array during each step. Thus
+ * reduced the size of the whole elements. Once one array reduced to 1 or 2
+ * we can manually find the correct element
  */
 public class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int start1 = 0;
-        int end1 = nums1.length;
-        int start2 = 0;
-        int end2 = nums2.length;
 
 
+        // Just consider the normal case
+        // The index of the target element(s) are (length - 1) / 2  or length / 2 for length > 0
+        // for example
+        // length  0  1  2  3  4  5  6  7  8  9  10
+        // index1  0
 
+        return (int) ((-1) / 2);
     }
 }
---
+++ leet/src/test/java/com/flybird/p004/SolutionTest.java
@@ -0,0 +1,18 @@
+package com.flybird.p004;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by hzhang.
+ */
+public class SolutionTest {
+    @Test
+    public void findMedianSortedArrays() throws Exception {
+        Solution solution = new Solution();
+
+        System.out.println(solution.findMedianSortedArrays(null, null));
+    }
+
+}
